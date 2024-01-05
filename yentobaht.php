ac54cbb293f16eb5dc994ce0713fe3a3ae4bc892
Anannapath <152575328+Anannapath@users.noreply.github.com>
2024-01-05 09:39:19 +0700

    yen to baht post
    yen to baht post

--- yentobaht.php
+++ yentobaht.php
@@ -1,7 +1,7 @@
 <php?
     $rate = 0.24;
-    $y = $_GET['yen'];
-    $bath = $y * $rate;
+    $y = $_POST['yen'];
+    $baht = $y * $rate;
 
-    echo "$y Yen = $bath Bath";
+    echo "$y Yen = $baht Baht";
 ?>
