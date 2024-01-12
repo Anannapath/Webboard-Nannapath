4bfdb7df34f330b5125868a990293c3cf3ffd0e8
Anannapath <s6603052423022@email.kmutnb.ac.th>
2024-01-12 10:05:56 +0700

    Up date verify.php
    Up date verify.php

--- verify.php
+++ verify.php
@@ -9,10 +9,29 @@
     <h1 style="text-align: center;">Webboard Nannapath</h1>
     <hr> 
     <div style="text-align: center;">
-        เข้าสู่ระบบด้วย<br>
-        Login = <?php echo $_POST['login']; ?> <br>
-        Password = <?php echo $_POST['pwd']; ?>
+
+    <?php
+        
+        $login=$_POST['login'];
+        $pwd=$_POST['pwd'];
+        if($login=="admin" && $pwd=="ad1234"){
+            echo "ยินดีต้อนรับคุณ ADMIN";
+        }elseif($login=="member" && $pwd=="mem1234"){
+            echo "ยินดีต้อนรับคุณ MEMBER";
+        }else{
+            echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";
+        }
+
+        // echo "เข้าสู่ระบบด้วย<br>";
+        // echo "Login = $_POST[login]<br>";
+        // echo "Password = $_POST[pwd]<br>";
+    ?>
+
     </div>
+    <center><a href="login.html">กลับไปยังหน้าหลัก</center>
+    
+    
+
 </body>
 </html>
 
