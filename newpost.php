7df506f7e30ccc998fe18c0b1e7e9da88a889326
Anannapath <152575328+Anannapath@users.noreply.github.com>
2024-01-19 11:33:46 +0700

    Updae newpost.php
    Updae newpost.php

---
+++ newpost.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['id'])){
+    header("location:index.php");
+    die();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1 style="text-align: center;">Webboard Nannapath</h1>
+    <hr>
+
+    <form action="">
+        <table>
+            <tr> <td>ผู้ใช้ : </td> <td> <?php echo $_SESSION['username']; ?> </td> </tr>
+            <tr><td>หมวดหมู่ :</td> <td>
+                    <select valoue="category">
+                    <option value="all">--ทั้งหมด--</option>
+                    <option value="general">เรื่องทั่วไป</option>
+                    <option value="study">เรื่องเรียน</option>
+                    </select>
+                    </td> </tr>
+            <tr><td>หัวข้อ :</td><td><input></td></tr>
+            <tr><td>เนื้อหา :</td><td><textarea  name="" id="" cols="40" rows="5"></textarea></td></tr>
+            <tr><td></td><td><input type="submit" value="บันทึกข้อความ"></td></tr>
+            
+        </table>
+    </form>
+
+</body>
+</html>
