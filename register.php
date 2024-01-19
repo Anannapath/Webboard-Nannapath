c2956cee3b1e8ec173456be472c4240ae7963e8a
Anannapath <152575328+Anannapath@users.noreply.github.com>
2024-01-19 11:42:57 +0700

    Update title
    Update title

--- register.php
+++ register.php
@@ -11,7 +11,7 @@ if(isset($_SESSION['id'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register</title>
 </head>
 <body>
     <h1 style="text-align: center;">สมัครสมาชิก</h1>
