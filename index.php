7acb04991d02d439a263cac56bb9079bc8b381b9
Anannapath <152575328+Anannapath@users.noreply.github.com>
2024-02-09 11:07:31 +0700

    Bootstrap Connext Download
    Download Bootstrap

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.2.3-dist/css/bootstrap.min.css">
+    <script src="bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js" ></script>
     <title>Webboard Nannapath</title>
 </head>
 <body>
