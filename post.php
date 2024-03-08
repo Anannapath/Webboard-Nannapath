ee30612268437c1fcff644da67bb156a9aa82759
Anannapath <152575328+Anannapath@users.noreply.github.com>
2024-03-08 11:47:32 +0700

    Edit post work10

--- post.php
+++ post.php
@@ -1,38 +1,54 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['id'])){
+    header("location:index.php");
+    die();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Post</title>
 </head>
 <body>
-<h1 style="text-align: center;">Webboard Nannapath</h1>
-    <hr> 
-    <div style="text-align: center;">
-        ต้องการดูกระทู้หมายเลข <?php echo " $_GET[id]<br>";
+    <div class="container-lg">
+        <h1 style="text-align: center;" class="mt-3">Webboard Nannapath</h1> 
 
-        $id = $_GET['id'];
-        if(($id%2) == 0){
-            echo "เป็นกระทู้หมายเลขคู่";
-        }else{
-            echo "เป็นกระทู้หมายเลขคี่";
-        }
-        ?>
-        
+        <?php
+            include "nav.php"
+        ?> 
+        <div class="row mt-4">
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
+            <div class="col-lg-6 col-md-8 col-sm-10">
+                <div class="card border-success mt-3">
+                    <div class="card-header bg-success text-white"> แสดงความคิดเห็น</div>
+                    <div class="card-body">
+                        <form action="post_save.php" method="post">
+                            <input type="hidden" name="post_id" value="<?= $_GET['id']; ?>">
+                            <div class="row mb-3 justify-content-center">
+                                <div class="col-lg-10">
+                                    <textarea name="comment" rows="8" class="form-control" require></textarea>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-12 d-flex justify-content-center" >
+                                    <button type="submit" class="btn btn-success btn-sm text-while">  <i class="bi bi-box-arrow-up-right"></i> ส่งข้อความ </button>
+                                    <button type="reset" class="btn btn-danger btn-sm ms-2">  <i class="bi bi-x-square"></i> ยกเลิก </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <br>
+            </div>
+            <div class="col-lg-3 col-md-2 col-sm-1 "></div>
+        </div>
     </div>
-    <br>
-    <table style="border: 2px solid black; width: 40%;" align="center">
-        <tr>
-            <td style=" background-color: #6CD2FE;">แสดงความคิดเห็น</td>
-        </tr> 
-        <tr>
-            <td> <center><textarea  name="" id="" cols="80" rows="10"></textarea></center> </td>
-        <tr>
-        <td style="text-align: center;"> 
-            <input type="submit" value="ส่งข้อความ"> </td>
-        </td>
-    </table>
-    <br>
-    <center><a href="index.php">กลับไปหน้าหลัก</center>
 </body>
 </html>
