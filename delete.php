c1e4d2ce1fa7069af17205310a7ea23b7e61068b
Anannapath <152575328+Anannapath@users.noreply.github.com>
2024-03-31 19:28:17 +0700

    Update delete.php delete user

--- delete.php
+++ delete.php
@@ -1,23 +1,32 @@
 <?php
+    session_start();
 
-session_start();
+    if(isset($_SESSION['id'])){
+        $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+        $sql = "SELECT p.user_id FROM post as p
+                WHERE p.id = {$_GET['id']}";
+         $result = $conn->query($sql);
+         $row = $result->fetch();
 
-if(isset($_SESSION['id']) && $_SESSION['role'] == 'a'){
+        if($_SESSION['role'] == 'a' || $_SESSION['user_id'] == $row['user_id']){
+            $id = $_GET['id'];
 
-    $id = $_GET['id'];
-      //echo " ลบกระทู้ หมายเลข $id ";
-      $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
-      $sql="DELETE FROM post WHERE id=$id"; 
-      $conn->exec($sql);
-      $sql="DELETE FROM comment WHERE post_id=$id";
-      $conn->exec($sql);
-      $conn=null;
-      header("location:index.php");
-      die();
+            $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+            $sql = "DELETE FROM post WHERE id = $id";
+            $conn->exec($sql);
 
-}else{
-    header("location:index.php");
-    die();
-}
-
+            $sql = "DELETE FROM comment WHERE post_id = $id";
+            $conn->exec($sql);
+            $conn = null;
+            header("location:index.php");
+            die();
+        }else{
+            header("location:index.php");
+            die();
+        }
+    }else{
+        header("location:index.php");
+        die();
+    }
+    
 ?>
